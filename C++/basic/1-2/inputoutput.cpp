96ea2de7acbaefbd0b056be30c79206941a25bea
mungon <74732825+kim-mun-gon@users.noreply.github.com>
2022-12-20 19:37:02 +0900

    Add files via upload

---
+++ C++/basic/1-2/inputoutput.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+    using namespace std;
+
+    int numOfWatermelon = 0;
+    cout << "How mant watermelons do you have? " << endl;
+    cin >> numOfWatermelon;
+
+    cout << "I have " << numOfWatermelon << endl; 
+    return 0;
+}
