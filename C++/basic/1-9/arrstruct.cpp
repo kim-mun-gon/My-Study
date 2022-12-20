05177e4c4ccc20eb22ca903dbfab5f020fba58ee
mungon <74732825+kim-mun-gon@users.noreply.github.com>
2022-12-20 19:50:03 +0900

    Add files via upload

---
+++ C++/basic/1-9/arrstruct.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+struct Person
+{
+    string name;
+    int age;
+};
+
+int main()
+{
+    Person students[2] = {
+        {"John Doe", 20},
+        {"Jane", 23}
+    };
+
+    cout << students[0].name << endl;
+    cout << students[1].name << endl;
+
+    cout << students[0].age << endl;
+    cout << students[1].age << endl;
+
+    return 0;
+}
---
+++ C++/basic/1-9/struct.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+struct  Person
+{
+    string name;
+    int age;
+};
+
+int main()
+{
+    Person p1;
+    p1.name = "Jonge Doe";
+    p1.age = 20;
+
+    Person p2 = {"Jane", 30};
+    cout << p1.name << endl;
+    cout << p2.name << endl;
+
+    return 0;
+}
