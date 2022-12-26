b120615545b8837e75c4dd7d0513632e678dc446
mungon <74732825+kim-mun-gon@users.noreply.github.com>
2022-12-26 22:57:09 +0900

    열거형 이해하기

---
+++ C/C언어 300제/45.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+enum {Sun = 0, Mon, Tue, Wed, Thr, Fri, Sat};
+
+int main(void)
+{
+    printf("%d", Sun);
+    printf("%d", Mon);
+    printf("%d", Tue);
+    printf("%d", Wed);
+    printf("%d", Thr);
+    printf("%d", Fri);
+    printf("%d", Sat);
+    return 0;
+}
