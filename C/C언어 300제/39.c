a56242393ffea56708d9d30a369038359961ba82
mungon <74732825+kim-mun-gon@users.noreply.github.com>
2022-12-26 22:53:57 +0900

    배열 이해하기

---
+++ C/C언어 300제/39.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int kor[10] = {100, 90, 35, 60, 75, 55, 95, 80, 90, 70};
+
+    for(int i = 0; i < 10; i++)
+        printf("%d ", kor[i]);
+
+    return 0;
+}
