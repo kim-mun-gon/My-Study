e24636b142eac244bb50949582ab6ad4c6c7ba9c
mungon <74732825+kim-mun-gon@users.noreply.github.com>
2023-01-09 23:44:35 +0900

    2차원 배열의 포인터 사용하기

---
+++ C/C언어 300제/114.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int jumsu[100][3];
+    int total[3] = {0, };
+    int (*pjumsu)[3];
+
+    pjumsu = jumsu;
+
+    for(int i = 0; i < 100; i++)
+    {
+        pjumsu[i][0] = 92;
+        pjumsu[i][1] = 90;
+        pjumsu[i][2] = 95;
+    }
+
+    for(int i = 0; i < 100; i++)
+    {
+        total[0] += pjumsu[i][0];
+        total[1] += pjumsu[i][1];
+        total[2] += pjumsu[i][2];
+    }
+
+    printf("국어 점수의 총점: %d \n", total[0]);
+    printf("영어 점수의 총점: %d \n", total[1]);
+    printf("수학 점수의 총점: %d \n", total[2]);
+
+    return 0;
+}
