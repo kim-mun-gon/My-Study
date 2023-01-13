1dff11cad231729633cc281cafd027e0aa9857b0
mungon <74732825+kim-mun-gon@users.noreply.github.com>
2023-01-13 18:57:14 +0900

    Add files via upload

---
+++ JAVASCRIPT/practice use doit js/05/js/quiz-2.js
@@ -0,0 +1,19 @@
+const num1 = document.querySelector("#number1");
+const num2 = document.querySelector("#number2");
+const button = document.querySelector("button");
+let result = document.querySelector("#result");
+
+button.onclick = function(){
+    result.innerText = getGCD(num1.value, num2.value);
+}
+
+function getGCD(num1, num2) {
+    let max = num1 > num2 ? num1 : num2;
+    let GCD = 0;
+    for (let i = 1; i <= max; i++) {
+      if (num1 % i === 0 && num2 % i === 0) {
+        GCD = i;   
+      }
+    }
+    return GCD;
+  }
